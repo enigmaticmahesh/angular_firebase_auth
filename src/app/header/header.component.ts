8ea8fd9f16e389ec8f9f021139fb44c7798fb28f
enigmaticmahesh <challengermahesh@gmail.com>
2022-10-31 20:56:00 +0530

    Added User Data in the Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/compat/auth";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-    isUserLoggedIn: boolean = false
+  isUserLoggedIn: boolean = false;
+  userDisplayName: string | undefined | null = '';
 
-    constructor(private auth: AngularFireAuth, private router: Router) {
-        
-    }
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
-    ngOnInit(): void {
-        this.auth.onAuthStateChanged(user => {
-            this.isUserLoggedIn = user ? true : false
-        })
-    }
+  ngOnInit(): void {
+    this.auth.onAuthStateChanged((user) => {
+      this.userDisplayName = user?.displayName;
+      this.isUserLoggedIn = user ? true : false;
+    });
+  }
 
-    signOut() {
-        this.auth.signOut()
-        .then(() => {
-            this.router.navigate(['/'])
-        })
-        .catch(err => console.log('User signing out error: ', err))
-    }
+  signOut() {
+    this.auth
+      .signOut()
+      .then(() => {
+        this.router.navigate(['/']);
+      })
+      .catch((err) => console.log('User signing out error: ', err));
+  }
 }
